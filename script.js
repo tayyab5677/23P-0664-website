1b9950ed4e5ee346afc5ef36dbc6251aacb887b4
Muhammad Tayyab <tayyabasghar299@gmail.com>
2025-10-25 21:10:19 +0500

    Initial commit - added website files

---
+++ script.js
@@ -0,0 +1,6 @@
+newFunction();
+function newFunction() {
+    document.getElementById("msgButton").addEventListener("click", function () {
+        alert("Hello! Weclome to my profile page.");
+    });
+}
